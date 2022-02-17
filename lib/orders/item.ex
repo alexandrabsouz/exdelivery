9a3c9980e9069898983f147399cdde6b2b226856
alexandra <alexandrabsouz@gmail.com>
2022-02-17 13:01:03 -0300

    feat: decimal lib

--- lib/orders/item.ex
+++ lib/orders/item.ex
@@ -16,6 +16,16 @@ defmodule Exlivery.Orders.Item do
 
   def build(description, category, unity_price, quantity)
       when quantity > 0 and category in @categories do
+        unity_price
+        |> Decimal.cast()
+        |> build_item(description, category, quantity)
+  end
+
+  def build(_description, _category, _unity_price, _quantity) do
+    {:error, "inválid parameters"}
+  end
+
+  defp build_item({:ok, unity_price}, description, category, quantity) do
     {:ok,
      %__MODULE__{
        description: description,
@@ -25,7 +35,8 @@ defmodule Exlivery.Orders.Item do
      }}
   end
 
-  def build(_description, _category, _unity_price, _quantity) do
-    {:error, "inválid parameters"}
-  end
+  defp build_item(:error, description, category, quantity), do: {:error, "inválid price"}
 end
+
+# alias Exlivery.Orders.Item
+# Item.build("Pizza de Peperoni", :pizza, 50.00, 1)
