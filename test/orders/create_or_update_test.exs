8602c7ef561bd5dd4438387cef9fbbe0ec975e4f
alexandra <alexandrabsouz@gmail.com>
2022-02-20 17:26:56 -0300

    test: agent order

---
+++ test/orders/create_or_update_test.exs
@@ -0,0 +1,45 @@
+# defmodule Exlivery.Users.CreateOrUpdateTest do
+#     use ExUnit.Case
+
+#     alias Exlivery.Users.Agent, as: UserAgent
+#     alias Exlivery.Users.CreateOrUpdate, as: CreateOrUpdateUser
+
+#     describe "call/1" do
+#       setup do
+#         UserAgent.start_link(%{})
+#         :ok
+#       end
+
+#       test "when all params are valid, saves the user" do
+#         params = %{
+#           name: "Alexandra Souza",
+#           address: "Rua do Infinito",
+#           email: "alexa@email.com",
+#           cpf: "00000000000",
+#           age: 23
+#         }
+
+#         response = CreateOrUpdateUser.call(params)
+
+#         expected_response = {:ok, "user info has been saved"}
+
+#         assert response == expected_response
+#       end
+
+#       test "when  params are valid, saves the user" do
+#         params = %{
+#           name: "Alexandra Souza",
+#           address: "Rua do Infinito",
+#           email: "alexa@email.com",
+#           cpf: "00000000000",
+#           age: 10
+#         }
+
+#         response = CreateOrUpdateUser.call(params)
+
+#         expected_response = {:error, "inválid parameters"}
+
+#         assert response == expected_response
+#       end
+#     end
+#   end
